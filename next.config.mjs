cfc324830bfbe9e25c8f1e8eeb63100902d56633
Vaishnavi Pangare <127540530+Vaishnavi639@users.noreply.github.com>
2025-09-28 19:39:38 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   // Allow cross-origin dev requests for internal assets when developing via external origins
   allowedDevOrigins: [
     'f7a7055a1f6f4a818faca4987cda3fcb-6081125f-181b-49cc-83ab-21c31a.fly.dev',
